618f4582691b96a11e07ff22768b1e6348d886da
Mukunda Bam <bammukunda1@gmail.com>
2026-05-12 16:17:32 +0545

    refactor: refactor the input and searchbox

--- src/components/Todoinput.js
+++ src/components/Todoinput.js
@@ -1,31 +1,39 @@
-import React, {useState} from 'react'
-import './Todoinput.css'
+import React, { useState } from "react";
+import "./Todoinput.css";
 
 function Todoinput(props) {
- const [inputText,setInputText] = useState()
- const handleEnterPress=(e)=>{
-  if (e.keyCode===13)
-    props.addList(inputText)
-  setInputText("")
- }
+  const [inputText, setInputText] = useState("");
+  
+  const handleEnterPress = (e) => {
+    if (e.keyCode === 13) {
+      props.addList(inputText);
+    setInputText("");
+    }
+  };
 
   return (
-   <div className='inputcontainer'>
-    <input type='text' className="input-box-todo" placeholder="Enter your todo list..." 
-    vlaue={inputText} onChange={e=>{
-        setInputText(e.target.value)
-    }}
-    onKeyDown={handleEnterPress}
-    ></input>
-    <button className="add-btn"
-     onClick={()=>{
-      props.addList(inputText)
-      setInputText("")
-    }}>+</button>
-    
-   </div>
-
-  )
+    <div className="inputcontainer">
+      <input
+        type="text"
+        className="input-box-todo"
+        placeholder="Enter your todo list..."
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+        onKeyDown={handleEnterPress}
+      ></input>
+      <button
+        className="add-btn"
+        onClick={() => {
+          props.addList(inputText);
+          setInputText("");
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
 }
 
-export default Todoinput
+export default Todoinput;
